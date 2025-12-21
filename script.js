e634daf1364d98ab5be5c8beb532938e68f96ff9
Sachinthaka Ravimal <universityravimal@gmail.com>
2025-12-22 03:20:09 +0530

    I add a logic for gameover and animation for that.

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ var walkAnimationNumber = 0;
 function walkAnimation() {
 
 
-        walkImageNumber = walkImageNumber + 1;
+    walkImageNumber = walkImageNumber + 1;
 
     if (walkImageNumber == 14) {
         walkImageNumber = 1;
@@ -61,18 +61,18 @@ function keyCheck(event) {
         }
 
         if (enemyAnimationId == 0) {
-            enemyAnimationId = setInterval(enemyAnimation,100);
+            enemyAnimationId = setInterval(enemyAnimation, 100);
         }
     }
 
-    if(keyCode == 32) {
-        if(jumpAnimationNumber == 0) {
+    if (keyCode == 32) {
+        if (jumpAnimationNumber == 0) {
             jumpAnimationStart();
         }
-        
+
     }
-    
-    
+
+
 
 
 }
@@ -84,8 +84,8 @@ var moveBackgroundId = 0;
 function moveBackground() {
 
     backgroundImagePositionX = backgroundImagePositionX - 10;
-
-    moveBackgroundId = document.getElementById("background").style.backgroundPositionX = backgroundImagePositionX + "px";
+    document.getElementById("background").style.backgroundPositionX = backgroundImagePositionX + "px";
+    
 }
 
 
@@ -96,19 +96,19 @@ var santaMarginTop = 450;
 function jumpAnimation() {
     jumpImageNumber = jumpImageNumber + 1;
 
-    santa.src = "resources/jump ("+ jumpImageNumber +").png";
+    santa.src = "resources/jump (" + jumpImageNumber + ").png";
 
-    if( jumpImageNumber <= 8) {
-        santaMarginTop = santaMarginTop - 20;
+    if (jumpImageNumber <= 8) {
+        santaMarginTop = santaMarginTop - 25;
         santa.style.marginTop = santaMarginTop + "px";
     }
-    
-    if( jumpImageNumber >=  9 ) {
-        santaMarginTop = santaMarginTop + 20;
+
+    if (jumpImageNumber >= 9) {
+        santaMarginTop = santaMarginTop + 25;
         santa.style.marginTop = santaMarginTop + "px"
     }
 
-    if(jumpImageNumber == 16) {
+    if (jumpImageNumber == 16) {
         clearInterval(jumpAnimationNumber);
         jumpImageNumber = 0;
         jumpAnimationNumber = 0;
@@ -118,40 +118,70 @@ function jumpAnimation() {
 }
 
 function jumpAnimationStart() {
-    clearInterval(idleAnimationNumber); 
-    walkImageNumber =0;
+    clearInterval(idleAnimationNumber);
+    walkImageNumber = 0;
     clearInterval(walkAnimationNumber);
-    jumpAnimationNumber = setInterval(jumpAnimation, 50);
+    jumpAnimationNumber = setInterval(jumpAnimation, 75);
 }
 
 var enemyMarginLeft = 1700;
-var reduceMargin = 1500;
 
 function createEnemy() {
-    for( var i =0; i <= 10; i++) {
+    for (var i = 0; i < 20; i++) {
 
-    var enemy =  document.createElement("div");
-    enemy.className="enemy";
-    document.getElementById("background").appendChild(enemy);
-    enemy.style.marginLeft = enemyMarginLeft + "px";
-    enemy.id = "enemy" + i;
+        var enemy = document.createElement("div");
+        enemy.className = "enemy";
+        document.getElementById("background").appendChild(enemy);
+        enemy.style.marginLeft = enemyMarginLeft + "px";
+        enemy.id = "enemy" + i;
 
-    for(var x=0; x<=i; x++) {
-        reduceMargin = reduceMargin - 20;
-    }
-    
-    enemyMarginLeft = enemyMarginLeft + reduceMargin;
+        if (i <= 14) {
+            enemyMarginLeft = enemyMarginLeft + 1050;
+        }
+        if (i > 14) {
+            enemyMarginLeft = enemyMarginLeft + 550;
+        }
 
     }
 }
 
 var enemyAnimationId = 0;
+var enemyCreateId = 0;
 
 function enemyAnimation() {
-    for( var i=0; i<10; i++) {
+    for (var i = 0; i < 20; i++) {
         var enemy = document.getElementById("enemy" + i);
         var currentMarginLeft = getComputedStyle(enemy).marginLeft;
-        var newMarginLeft = parseInt(currentMarginLeft) - 25;
-        enemy.style.marginLeft = newMarginLeft +"px"; 
+        var newMarginLeft = parseInt(currentMarginLeft) - 40;
+        enemy.style.marginLeft = newMarginLeft + "px";
+
+        //logic for game over.
+        if (newMarginLeft >= 150 && newMarginLeft <= 350) {
+            if (santaMarginTop > 400) {
+                clearInterval(enemyAnimationId);
+                clearInterval(walkAnimationNumber);
+                walkAnimationNumber = -1;
+                clearInterval(jumpAnimationNumber);
+                jumpAnimationNumber = -1;
+                clearInterval(moveBackgroundId);
+                moveBackgroundId = -1;
+
+                deathAnimationNumber = setInterval(deathAnimation,100);
+            }
+        }
     }
+}
+
+var deathImageNumber =1;
+var deathAnimationNumber = 0;
+
+function deathAnimation() {
+
+    deathImageNumber = deathImageNumber + 1;
+
+    if(deathImageNumber == 18) {
+        deathImageNumber =17;
+    }
+    
+    santa.src = "resources/dead (" + deathImageNumber +").png";
 }
